dd51ef86381b4f3425af883938285163a9b50b87
JayArindamMaity <maityjayarindam@gmail.com>
2025-06-22 11:51:37 +0530

    wallpaper fixes

--- src/data/wallpapers.ts
+++ src/data/wallpapers.ts
@@ -1,22 +1,22 @@
 export const wallpapers = [
   {
     title: "Anime Girl",
-    imageUrl: "src/data/wallpapers/anime-girl.jpg",
+    imageUrl: "/wallpapers/anime-girl.jpg",
     downloadName: "Anime Girl",
   },
   {
     title: "Pop Art",
-    imageUrl: "src/data/wallpapers/pop-art.jpg",
+    imageUrl: "/wallpapers/pop-art.jpg",
     downloadName: "Pop Art",
   },
   {
     title: "Blood Mage",
-    imageUrl: "src/data/wallpapers/blood-mage.jpeg",
+    imageUrl: "/wallpapers/blood-mage.jpeg",
     downloadName: "Blood Mage",
   },
   {
     title: "Makima",
-    imageUrl: "src/data/wallpapers/makima.jpg",
+    imageUrl: "/wallpapers/makima.jpg",
     downloadName: "Makima",
   },
 ];
