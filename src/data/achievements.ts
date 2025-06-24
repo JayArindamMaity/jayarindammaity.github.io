f9b7a84f2653a80c7b7db779324f1962849bcce1
JayArindamMaity <maityjayarindam@gmail.com>
2025-06-24 12:38:43 +0530

    additions

--- src/data/achievements.ts
+++ src/data/achievements.ts
@@ -15,6 +15,30 @@ const featured_cert = [
 
 const all_certificates = [
   {
+    name: "Introduction to Cloud Computing",
+    organisation: "IBM",
+    issuedate: "April 2024",
+    certlink: "https://coursera.org/share/1ad2a449d5769e77201a4cd265388aae",
+  },
+  {
+    name: "Cloud Data Engineering",
+    organisation: "Duke University",
+    issuedate: "May 2024",
+    certlink: "https://coursera.org/share/c651d860c84f2f61d1987e805bfaeb90",
+  },
+  {
+    name: "Introduction to Social Media Marketing",
+    organisation: "Meta",
+    issuedate: "May 2024",
+    certlink: "https://coursera.org/share/17988b5e1ac7d766639015676b2775da",
+  },
+  {
+    name: "Advertising with Meta",
+    organisation: "Meta",
+    issuedate: "May 2024",
+    certlink: "https://coursera.org/share/c21278a5358b4845b248b8d2342ccc0c",
+  },
+  {
     name: "Introduction to Artificial Intelligence (AI)",
     organisation: "IBM",
     issuedate: "October 2024",
@@ -25,6 +49,12 @@ const all_certificates = [
     organisation: "IBM",
     issuedate: "October 2024",
     certlink: "https://coursera.org/share/abb1ff9b3960763cba11551ee2a86163",
+  },
+  {
+    name: "Generative AI: Prompt Engineering Basics",
+    organisation: "IBM",
+    issuedate: "October 2024",
+    certlink: "https://coursera.org/share/2620e6d9bc6bcc27fa0c8a76d844a5be",
   },
   {
     name: "Supervised Machine Learning: Regression",
@@ -44,6 +74,36 @@ const all_certificates = [
     issuedate: "March 2025",
     certlink: "https://coursera.org/share/ae279b1807091808775a55138a297bd3",
   },
+  {
+    name: "Deep Learning and Reinforcement Learning",
+    organisation: "IBM",
+    issuedate: "March 2025",
+    certlink: "https://coursera.org/share/12510298d790b622f2d2be6231810697",
+  },
+  {
+    name: " Unsupervised Machine Learning",
+    organisation: "IBM",
+    issuedate: "March 2025",
+    certlink: "https://coursera.org/share/425d6521d7d49871dbef88c59d268fd1",
+  },
+  {
+    name: "The Art of the Job Interview",
+    organisation: "Big Interview",
+    issuedate: "March 2025",
+    certlink: "https://coursera.org/share/df04ad686419ac0cf6fd5b4d65827017",
+  },
+  {
+    name: "Coding Interview Preparation",
+    organisation: "Meta",
+    issuedate: "May 2025",
+    certlink: "https://coursera.org/share/8bcdb84ef919eddab942b2034fdc0a42",
+  },
+  // {
+  //     name : "",
+  //     organisation: "",
+  //     issuedate : "",
+  //     certlink: "",
+  // }
 ];
 
 export { all_certificates, featured_cert };
