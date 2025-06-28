606c1a80a55120284e47bde51c7428739c81e4b5
JayArindamMaity <maityjayarindam@gmail.com>
2025-06-28 13:11:18 +0530

    added data

--- src/data/entertainment.ts
+++ src/data/entertainment.ts
@@ -8,23 +8,23 @@ import type { Movie, Song, Anime, Game } from "./types";
 const anime: Anime[] = [
   {
     aniname: "Hellsing Ultimate",
-    studio: "",
+    studio: "Madhouse",
     watchlink: "",
   },
   {
     aniname: "Attack on Titan",
-    studio: "",
+    studio: "Mappa",
     watchlink: "",
   },
   {
     aniname: "SpyXFamily",
-    studio: "",
-    watchlink: "",
+    studio: "CloverWorks",
+    watchlink: "https://www.youtube.com/watch?v=h_tL16PZ0IE&list=PLwLSw1_eDZl1wGMYg5oB3uEns0CZNl6sI",
   },
   {
     aniname: "One Punch Man",
-    studio: "",
-    watchlink: "",
+    studio: "J.C.Staff",
+    watchlink: "https://www.youtube.com/watch?v=VvK6UZWNU2Q&list=PLwLSw1_eDZl2XdtLhB9NG2Ch050jWFm9G",
   },
 ];
 
@@ -39,33 +39,53 @@ const songs: Song[] = [
   {
     songname: "The Scientist",
     artist: "Coldplay",
-    spotify: "",
+    spotify: "https://open.spotify.com/track/75JFxkI2RXiU7L9VXzMkle",
   },
   {
     songname: "Castle on the Hill",
     artist: "Ed Sheeran",
-    spotify: "",
+    spotify: "https://open.spotify.com/track/6PCUP3dWmTjcTtXY02oFdT",
   },
   {
     songname: "High and Dry",
     artist: "Radiohead",
-    spotify: "",
+    spotify: "https://open.spotify.com/track/2a1iMaoWQ5MnvLFBDv4qkf",
   },
   {
     songname: "Silvitni",
     artist: "Eivor",
-    spotify: "",
+    spotify: "https://open.spotify.com/track/0DtktYF0tDTCOEGGrUjCRb",
   },
   {
     songname: "Fade Into You",
     artist: "Mazzy Star",
-    spotify: "",
+    spotify: "https://open.spotify.com/track/1LzNfuep1bnAUR9skqdHCK",
   },
   {
     songname: "Delicate Weapon",
     artist: "Grimes",
-    spotify: "",
+    spotify: "https://open.spotify.com/track/1hT3eaGzrcFriQtgGdvsZv",
   },
+  {
+    songname: "Labour",
+    artist: "Paris Paloma",
+    spotify: "https://open.spotify.com/track/0e00DiF2T9znEdmWakYSC3",
+  },
+  {
+    songname: "Cocktail Molotov",
+    artist: "ZAND",
+    spotify: "https://open.spotify.com/track/4zSeX6VgtIZHLgIz2hJT3p",
+  },
+  {
+    songname: "Slipping",
+    artist: "Crysoshell",
+    spotify: "https://open.spotify.com/track/7piKVALJFj1tIGKI1E47FP",
+  },
+  // {
+  //   songname: "",
+  //   artist: "",
+  //   spotify: ""
+  // }
 ];
 
 // ------------------------ this is the sample data for sugested movies
