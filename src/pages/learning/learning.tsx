af6e9ab323b76df1a99e487b35be3e5e924a6272
JayArindamMaity <maityjayarindam@gmail.com>
2025-06-30 20:05:31 +0530

    more additions

--- src/pages/learning/learning.tsx
+++ src/pages/learning/learning.tsx
@@ -1,9 +1,25 @@
-import './learning.css'
+import "./learning.css";
 
-export default function Learning () {
-    return (
-        <>
-            this is where you can see some of my most favourite books
-        </>
-    )
+export default function Learning() {
+  return (
+    <>
+      <div className="l-main">
+        <div className="l-intro">
+          <div className="l-heading">Learning Section...</div>
+          <div className="l-content">
+            Welcome to the learning section of my website...
+            <br /> <br />
+            This is where I shall share the resources and stuff that you as well
+            can use to learn new things...
+            <br />
+            Most of these things will be realated to computer science and might
+            be very boring to many of you. But if you keep perusing you might
+            find something of interest... 
+            <br />This may include some really useaful git repositories, video links, books, articles and more...
+            <br /> <br /> HAPPY LEARNING!!! 😁
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
